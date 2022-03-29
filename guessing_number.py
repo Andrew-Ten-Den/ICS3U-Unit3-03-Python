e945117db193be344a95e271f1485b590137fc94
Andrew-Ten-Den <99287083+Andrew-Ten-Den@users.noreply.github.com>
2022-03-29 18:23:48 +0000

    fixed

--- guessing_number.py
+++ guessing_number.py
@@ -4,30 +4,27 @@
 # Created on: March 2022
 # This program lets the user guess a number between 0 and 9
 
-
 import random
-
-guess_variable = random.randint(0, 9)  # a number between 1 and 100
 
 
 def main():
     # this function lets the user guess a number between 0 and 9
 
+  guess_variable = random.randint(0, 9)  # a number between 1 and 100
+
     # input
-    number_guessed = int(input("Enter a number between 0 and 9: "))
+  number_guessed = int(input("Enter a number between 0 and 9: "))
 
     # process & output
-    if number_guessed == guess_variable:
+  if number_guessed == guess_variable:
         print("You guessed correct!")
-        print("")
-        print("Done")
 
-    if number_guessed != guess_variable:
+  if number_guessed != guess_variable:
         print("You guessed wrong!")
         print("The number was {}.".format(guess_variable))
-        print("")
-        print("Done")
 
+  print("")
+  print("Done")
 
 if __name__ == "__main__":
     main()
